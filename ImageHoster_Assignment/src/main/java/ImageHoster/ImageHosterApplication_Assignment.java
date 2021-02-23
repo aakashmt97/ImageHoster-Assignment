b8e7bf3d17b85ba9cba671a112d381dec571b884
aakashmt97 <tiwari.aakashm.5496@gmail.com>
2021-02-23 14:07:51 +0530

    ImageHoster Application

---
+++ ImageHoster_Assignment/src/main/java/ImageHoster/ImageHosterApplication_Assignment.java
@@ -0,0 +1,11 @@
+package ImageHoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHosterApplication_Assignment {
+    public static void main(String[] args) {
+        SpringApplication.run(ImageHosterApplication_Assignment.class, args);
+    }
+}
